6da651a979aa8ec8fcd9838e05f436ed730eb875
klima3775 <113065590+klima3775@users.noreply.github.com>
2024-12-19 23:40:23 +0200

    Remove unused test and web vitals files; update backend dependencies and main server setup

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,1 +1,24 @@
-console.log("Hello Worldы!");
+import express, { Application, Request, Response, NextFunction } from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+// import transactionsRoutes from "./routes/transactions.js";
+// import userRoutes from "./routes/users.js";
+
+dotenv.config();
+
+const app: Application = express();
+
+const Port: number = (process.env.PORT && parseInt(process.env.PORT)) || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI || "")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+app.listen(Port, () => {
+  console.log(`Server is running on port ${Port}`);
+});
