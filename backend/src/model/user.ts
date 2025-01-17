b972f38f46462808b99c76ac871c9e8b7d0c2b70
klima3775 <113065590+klima3775@users.noreply.github.com>
2025-01-17 02:18:42 +0200

    Update user model to include token field and improve password hashing; add new dependencies

--- backend/src/model/user.ts
+++ backend/src/model/user.ts
@@ -1,27 +1,33 @@
-import mongoose, { Document, Schema } from "mongoose";
+import mongoose, { Document, model, Schema } from "mongoose";
 import bcrypt from "bcrypt";
 
 // Интерфейс для пользователя
 interface IUser extends Document {
-  name: string;
   email: string;
+  token: string;
   password: string;
 }
 
 // Схема пользователя
 const userSchema: Schema<IUser> = new mongoose.Schema({
-  name: { type: String, required: true }, // Имя пользователя
+  token: { type: String, required: true }, // Токен пользователя
   email: { type: String, required: true, unique: true }, // Email пользователя
-  password: { type: String, required: true }, // Хэшированный пароль
+  password: { type: String, required: true }, // Пароль пользователя
 });
 
-// Хэширование пароля перед сохранением
-userSchema.pre<IUser>("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
+// Хэширование пароля
+userSchema.pre("save", async function (next) {
+  const user = this as IUser;
+  if (!user.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+    next();
+  } catch (err) {
+    next(err as Error);
+  }
 });
 
-// Экспорт модели пользователя
-export default mongoose.model<IUser>("User", userSchema);
+const User = model<IUser>("User", userSchema);
+
+export default User;
