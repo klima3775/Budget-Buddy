7ef68968ea6e854df57ee3fa9eaaececa01ba72e
klima3775 <113065590+klima3775@users.noreply.github.com>
2025-01-20 00:27:02 +0200

    Add server listen functionality and log server status on startup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,3 +13,6 @@ const Port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use("/api/auth", router);
+app.listen(Port, () => {
+  console.log(`Server is running on port ${Port}`);
+});
