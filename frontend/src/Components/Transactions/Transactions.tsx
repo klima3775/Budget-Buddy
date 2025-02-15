f76187ed808a2641dcf63efb7ee72b455f5904cc
klima3775 <113065590+klima3775@users.noreply.github.com>
2025-02-15 03:27:10 +0200

    Add test block for frontend

---
+++ frontend/src/Components/Transactions/Transactions.tsx
@@ -0,0 +1,11 @@
+import "./Transactions.scss";
+
+const Transactions: React.FC = () => {
+  return (
+    <div className="transactions">
+      <h1>Test block transaction</h1>
+    </div>
+  );
+};
+
+export default Transactions;
--- frontend/src/pages/Cabinet/Cabinet.tsx
+++ frontend/src/pages/Cabinet/Cabinet.tsx
@@ -1,11 +1,12 @@
 import CardBlock from "../CardBlock/CardBlock";
 import SearchBar from "../SearchBar/SearchBar";
-
+import Transactions from "../../Components/Transactions/Transactions"; // test variant
 const Cabinet = () => {
   return (
     <div className="cabinet">
       <CardBlock />
       <SearchBar />
+      <Transactions />
     </div>
   );
 };
