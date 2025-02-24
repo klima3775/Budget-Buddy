ce7fad9078b9588f9d5ce75a4a6353edf962940d
klima3775 <113065590+klima3775@users.noreply.github.com>
2025-02-24 17:44:26 +0200

    Refactor auth middleware and client info service: improve error handling and integrate user authentication

--- backend/src/middleware/authMiddleware.ts
+++ backend/src/middleware/authMiddleware.ts
@@ -1,27 +1,22 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
-export interface AuthRequest extends Request {
-  user?: string | jwt.JwtPayload;
-}
-
 export const authMiddleware = (
-  req: AuthRequest,
+  req: Request,
   res: Response,
   next: NextFunction
-): void => {
+) => {
   const token = req.cookies.accessToken;
 
   if (!token) {
-    res.status(401).json({ message: "Неавторизований" });
-    return;
+    return res.status(401).json({ message: "Немає доступу" });
   }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
     req.user = decoded;
     next();
-  } catch (err) {
-    res.status(401).json({ message: "Неправильний або прострочений токен" });
+  } catch (error) {
+    return res.status(403).json({ message: "Недійсний токен" });
   }
 };
--- backend/src/services/clientInfo.ts
+++ backend/src/services/clientInfo.ts
@@ -1,8 +1,24 @@
 import monoClient from "../client/monoClient.js";
+import { Request, Response } from "express";
+import User from "../model/user.js";
+import { decryptToken } from "../utils/encription.js";
 
-export default async function fetchClientInfo() {
+export default async function fetchClientInfo(req: Request, res: Response) {
   try {
-    const response = await monoClient.get("/personal/client-info");
+    const userId = (req as any).user.id;
+    const user = await User.findById(userId);
+
+    if (!user || !user.token) {
+      return res.status(400).json({ message: "Користувач не авторизований" });
+    }
+
+    const token = decryptToken(user.token);
+
+    const response = await monoClient.get("/personal/client-info", {
+      headers: {
+        "X-Token": token,
+      },
+    });
     return response.data;
   } catch (error) {
     throw new Error("Помилка отримання інформації про клієнта");
