0deb5445c56c464edd5bace8d150085129cc4359
klima3775 <113065590+klima3775@users.noreply.github.com>
2025-03-02 00:06:52 +0200

    Add @tanstack/react-query for data fetching and wrap App component with QueryClientProvider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
 import {
   BrowserRouter as Router,
@@ -9,6 +10,8 @@ import "./App.css";
 import FormRegLog from "./Components/FormRefLog/FormRegLog";
 import AuthButtons from "./Components/AuthBtn/AuthBtn";
 import Cabinet from "./pages/Cabinet/Cabinet";
+
+const queryClient = new QueryClient();
 
 function App() {
   const [isLogin, setIsLogin] = useState<boolean | null>(null);
@@ -24,40 +27,42 @@ function App() {
   };
 
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route
-            path="/"
-            element={
-              isAuthenticated ? (
-                <Navigate to="/cabinet" />
-              ) : (
-                <AuthButtons
-                  onLoginClick={() => setIsLogin(true)}
-                  onRegisterClick={() => setIsLogin(false)}
-                />
-              )
-            }
-          />
-          <Route
-            path="/auth"
-            element={
-              isLogin !== null ? (
-                <FormRegLog
-                  onSubmit={handleSubmit}
-                  onCancel={handleCancel}
-                  isLogin={isLogin}
-                />
-              ) : (
-                <Navigate to="/" />
-              )
-            }
-          />
-          <Route path="/cabinet" element={<Cabinet />} />
-        </Routes>
-      </div>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                isAuthenticated ? (
+                  <Navigate to="/cabinet" />
+                ) : (
+                  <AuthButtons
+                    onLoginClick={() => setIsLogin(true)}
+                    onRegisterClick={() => setIsLogin(false)}
+                  />
+                )
+              }
+            />
+            <Route
+              path="/auth"
+              element={
+                isLogin !== null ? (
+                  <FormRegLog
+                    onSubmit={handleSubmit}
+                    onCancel={handleCancel}
+                    isLogin={isLogin}
+                  />
+                ) : (
+                  <Navigate to="/" />
+                )
+              }
+            />
+            <Route path="/cabinet" element={<Cabinet />} />
+          </Routes>
+        </div>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
