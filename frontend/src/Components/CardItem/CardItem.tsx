bad442f7977cddfa885d5671ea65daf6398667fc
klima3775 <113065590+klima3775@users.noreply.github.com>
2025-03-12 21:07:24 +0200

    Remove commented-out code from CardItem component for cleaner implementation

--- frontend/src/Components/CardItem/CardItem.tsx
+++ frontend/src/Components/CardItem/CardItem.tsx
@@ -1,27 +1,3 @@
-// import CardsType from "../../utils/cardsType";
-// import "./CardItem.scss";
-
-// const CardItem: React.FC<{ card: CardsType }> = ({ card }) => {
-//   const getCurrencySymbol = (currencyCode: number) => {
-//     return currencyCode === 980 ? "UAN" : "$";
-//   };
-
-//   return (
-//     <div className="card-item">
-//       <h3>{card.type}</h3>
-//       <p>
-//         {card.balance / 100} {getCurrencySymbol(card.currencyCode)}
-//       </p>
-//       <p>
-//         {card.creditLimit / 100} {getCurrencySymbol(card.currencyCode)}
-//       </p>
-//       <p>{card.maskedPan}</p>
-//     </div>
-//   );
-// };
-
-// export default CardItem;
-
 import React from "react";
 import CardsType from "../../utils/cardsType";
 import "./CardItem.scss";
