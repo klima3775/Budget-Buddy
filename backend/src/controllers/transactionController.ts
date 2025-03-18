f3da97049b1970a5b4af2d921681a59ef9e460d9
klima3775 <113065590+klima3775@users.noreply.github.com>
2025-03-18 15:24:04 +0200

    test varianr transaction

--- backend/src/controllers/transactionController.ts
+++ backend/src/controllers/transactionController.ts
@@ -5,9 +5,15 @@ import fetchStatement from "../services/fetchStatment.js";
 const getTransactions = async (req: Request, res: Response): Promise<void> => {
   try {
     const userId = (req as any).user.id;
-    const { account } = req.params;
+    const account = req.query.account as string;
+    console.log("req.params:", req.params);
+    console.log("req.query:", req.query);
+    console.log("req.body:", req.body);
+    console.log("userId:", userId);
+    console.log("account:", account);
 
     if (!userId || !account) {
+      console.log("Невірні параметри запиту");
       res.status(400).json({ message: "Невірні параметри запиту" });
       return;
     }
--- frontend/src/Components/CardItem/CardItem.tsx
+++ frontend/src/Components/CardItem/CardItem.tsx
@@ -1,40 +1,3 @@
-// import React from "react";
-// import CardsType from "../../utils/cardsType";
-// import "./CardItem.scss";
-
-// const CardItem: React.FC<{ card: CardsType }> = ({ card }) => {
-//   const getCurrencySymbol = (currencyCode: number) => {
-//     return currencyCode === 980 ? "UAN" : "$";
-//   };
-
-//   const copyToClipboard = (text: string) => {
-//     navigator.clipboard
-//       .writeText(text)
-//       .then(() => {
-//         alert("IBAN скопійовано в буфер обміну");
-//       })
-//       .catch((err) => {
-//         console.error("Помилка копіювання в буфер обміну:", err);
-//       });
-//   };
-
-//   return (
-//     <div className="card-item">
-//       <h3>{card.type}</h3>
-//       <p>
-//         {card.balance / 100} {getCurrencySymbol(card.currencyCode)}
-//       </p>
-//       <p>
-//         {card.creditLimit / 100} {getCurrencySymbol(card.currencyCode)}
-//       </p>
-//       <p>{card.maskedPan}</p>
-//       <button onClick={() => copyToClipboard(card.iban)}>Копіювати IBAN</button>
-//     </div>
-//   );
-// };
-
-// export default CardItem;
-
 import React, { useState } from "react";
 import CardsType from "../../utils/cardsType";
 import "./CardItem.scss";
@@ -54,13 +17,16 @@ const CardItem: React.FC<{ card: CardsType }> = ({ card }) => {
       .then(() => alert("IBAN скопійовано в буфер обміну"))
       .catch((err) => console.error("Помилка копіювання:", err));
   };
-
+  console.log("card.id:", card.id);
   const fetchTransactions = async () => {
     setLoading(true);
     setError(null);
+    console.log("card.id:", card.id);
     try {
+      console.log("card.id:", card.id);
       const response = await fetch(
         `http://localhost:5000/api/mono/statement?account=${card.id}`,
+
         {
           method: "GET",
           credentials: "include",
